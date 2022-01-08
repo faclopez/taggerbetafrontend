16bb957f35a8b0660082c1e88a70d1fdc839c455
faclopez <facundo.lopez@mercadolibre.com>
2022-01-08 13:57:29 -0300

    Changing tags

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -40,10 +40,14 @@ export const OrderUpdate = (props) => {
             <form onSubmit={handleSubmit}>
         
           <select value={tag} onChange={evt => setTag(evt.target.value)}>
-            <option value="casting">Casting</option>
-            <option value="emerying">Emerying</option>
-            <option value="soldering">Soldering</option>
-            <option value="polishing">Polishing</option>
+            <option value="">Select Tag</option>
+            <option value="Order Processed">Order Processed</option>
+            <option value="3D printed/Sent for casting">3D printed/Sent for casting</option>
+            <option value="Received Back From Casting">Received Back From Casting</option>
+            <option value="Into Production Studio">Into Production Studio</option>
+            <option value="Needs recast">Needs recast</option>
+            <option value="Design issue">Design issue</option>
+            <option value="Hold">Hold</option>
           </select>
         <button> Update Order </button>
         
